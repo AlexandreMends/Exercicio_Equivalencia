86efff76a36fb3552a7227dc913ccb27424544c6
Alexandre mendes <ale.alexandremendes@hotmail.com>
2022-05-13 21:03:02 -0300

    Revert "exercícios"
    This reverts commit 6ce66ffbea0fecb0df390f3df26ca74ba89c58c4.

--- src/main/java/inatel/cdg/br/Jogador/Jogador3.java
+++
@@ -1,9 +0,0 @@
-package inatel.cdg.br.Jogador;
-
-import inatel.cdg.br.Empresario.Empresario;
-
-public class Jogador3 extends Empresario {
-    public Jogador3 (String nome, Double preco){
-        super(nome, preco);
-    }
-}
